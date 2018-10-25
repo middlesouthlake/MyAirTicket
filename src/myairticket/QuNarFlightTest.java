6ef5dcdbd640665c88f31221efa905e889811dfc
middlesouthlake <middlesouthlake@gmail.com>
2018-10-25 18:50:33 -0400

    adding testcase

---
+++ src/myairticket/QuNarFlightTest.java
@@ -0,0 +1,59 @@
+package myairticket;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QuNarFlightTest {
+
+	@Test
+	void testSetFlightCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlightCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetListPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDecryptPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPriceFontName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPriceFontName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuildFontLibrary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDecodeFontFile() {
+		fail("Not yet implemented");
+	}
+
+}
